7c5b41d8ea164832b6e209851e0e0e22b1f11aca
Jonas Groh <groh.jonas@hm.edu>
2024-12-17 15:10:55 +0100

    testing

--- mein-chatbot/app.py
+++ mein-chatbot/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Setze deinen OpenAI API-Schlüssel
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = Key_1
 
 path_modulhandbuch = "./data"
 path_persist = os.path.join(path_modulhandbuch, "persist")
