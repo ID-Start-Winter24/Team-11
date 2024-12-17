4556c705ab5cc268cf803fd4e35bab39bc177652
Jonas Groh <groh.jonas@hm.edu>
2024-12-17 16:48:14 +0100

    port

--- mein-chatbot/app.py
+++ mein-chatbot/app.py
@@ -43,4 +43,4 @@ def chat():
     return jsonify({'response': str(response)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(port=int(os.environ.get('PORT', 5000)))
