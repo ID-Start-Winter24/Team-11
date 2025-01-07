f13491000eb3f55233a8cea64c507c1251603fc4
Jonas Groh <groh.jonas@hm.edu>
2025-01-07 12:56:02 +0100

    serverfix2

--- mein-chatbot/app.py
+++ mein-chatbot/app.py
@@ -1,18 +1,31 @@
 import os
 from flask import Flask, request, jsonify
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage, PromptTemplate
+from llama_index.llms.openai import OpenAI
 import openai
 from flask_cors import CORS
+import requests 
+
+reply = requests.post(url="https:/", json={"":""})
 
 app = Flask(__name__)
 CORS(app)
 
 # Setze deinen OpenAI API-Schlüssel
-openai.api_key = os.getenv('OPEN_AI_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Lade Dokumente und erstelle den Index
-documents = SimpleDirectoryReader('data').load_data()
-index = VectorStoreIndex(documents)
+path_modulhandbuch = "./data"
+path_persist = os.path.join(path_modulhandbuch, "persist")
+
+#print(openai.api_key)
+
+if not os.path.exists(path_persist):
+    documents = SimpleDirectoryReader("./data/").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=path_persist)
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=path_persist)
+    index = load_index_from_storage(storage_context)
 
 template = (
     "We have provided context information below. \n"
@@ -33,4 +46,4 @@ def chat():
     return jsonify({'response': str(response)})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
